91668dfbede7a285e96f1e0c80751dcac7fe80ff
Lucas Alves <lucashalves@live.com>
2017-09-23 15:04:06 -0300

    Updating some repositories

--- src/repositories/guessRepository.js
+++ src/repositories/guessRepository.js
@@ -1,14 +1,11 @@
 'use Strict';
 
-const Boom = require('boom');
-const qs = require('querystring')
-
 module.exports = (app) => {
   const requestManager = app.coincidents.Managers.requestManager;
   const apis = app.coincidents.Config.apis;
 
   const createGuessLeague = (reqBody, headers) => {
-    let uri = `${apis.guessUrl}/guessleague/create`
+    const uri = `${apis.guessUrl}/guessleague/create`
 
     return requestManager.post(uri, headers, reqBody)
       .catch((err) => {
--- src/repositories/personalRepository.js
+++ src/repositories/personalRepository.js
@@ -7,28 +7,26 @@ module.exports = (app) => {
   const requestManager = app.coincidents.Managers.requestManager;
   const apis = app.coincidents.Config.apis;
 
-  const getProfile = (reqBody, headers) => {
-    let uri = `${apis.personalUrl}/profiles/getProfile`
-    const urlQuerystring = qs.stringify({userName: reqBody.userName})
-    uri = `${uri}?${urlQuerystring}`
-    
+  const getProfile = (request, headers) => {
+    const uri = `${apis.personalUrl}/profiles/getProfile?${_buildQueryString(request)}`
+
     return requestManager.get(uri, headers)
-    .catch((err) => Boom.serverUnavailable(err));
+      .catch((err) => Boom.serverUnavailable(err));
   }
 
-  const sendGuessLeagueNotifications = (reqBody, headers) => {
-    let uri = `${apis.guessUrl}/notifications/setGuessLeagueNotifications`
+  const sendGuessLeagueNotifications = (request, headers) => {
+    const uri = `${apis.guessUrl}/notifications/setGuessLeagueNotifications`
 
-    return requestManager.put(uri, headers, reqBody)
+    return requestManager.put(uri, headers, request)
       .catch((err) => {
         throw err
       });
   }
 
 
-  const singIn = (reqBody) => {
-    // requestManager.post(`${apis.personalUrl}/getteams`, null, reqBody)
-    // .catch((err) => Boom.serverUnavailable(err));
+  const singIn = (request) => {
+    //requestManager.post(`${apis.personalUrl}/getteams`, null, request)
+    //.catch((err) => Boom.serverUnavailable(err));
   }
 
 
@@ -36,4 +34,18 @@ module.exports = (app) => {
     getProfile,
     singIn
   }
+}
+
+const _buildQueryString = (request) => {
+  const keys = Object.keys(request)
+  const value = Object.values(request)
+
+  keys.forEach((key, index) => {
+    
+  })
+  const qsBuilded = qs.stringify({
+    userRef: request.userRef
+  })
+  
+  return qsBuilded
 }
